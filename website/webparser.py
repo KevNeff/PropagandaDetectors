32fce363d4ba6e4b66fdb09a9e924081d11a43a8
KevNeff <kevin.mcneff@gmail.com>
2019-12-06 23:29:21 -0600

    Refactor
    Reorganized, reduce loaded memory, removed deprecated error handling

--- website/webparser.py
+++ website/webparser.py
@@ -1,54 +1,41 @@
 import sys
-from newspaper import Article
-import numpy as np
-import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer,TfidfTransformer
-import nltk
-from nltk.stem import PorterStemmer
-from nltk.corpus import stopwords
 
-argerror = -1
-if sys.argv[0] == 0:
-    print('URL Error')
+def url_miner():
+    from newspaper import Article
+    try:
+        article = Article(sys.argv[1])
+        article.download()
+        article.parse()
+    except:
+        print('Invalid URL')
+        sys.stdout.flush()
+        exit()
+    article = article.text.split('\n')
+    return article
+
+def modelLoader():
+    import pickle
+    loaded_vec = TfidfVectorizer(stop_words='english',lowercase=True,vocabulary=pickle.load(open("feature.pkl", "rb")))
+    with open("trial.pkl", 'rb') as file:
+        pickle_model = pickle.load(file)
+    return loaded_vec, pickle_model
+
+def inject_to_server(predictions, corpus):
+    count = 0
+    for prediction in predictions:
+        if prediction == 'propaganda':
+            print(corpus[count] + '\n')
+        count += 1
     sys.stdout.flush()
     exit()
 
-sys.stdout.flush()
+def Main():
+    word_vector, model = modelLoader()
+    corpus = url_miner()
+    samples = word_vector.fit_transform(corpus)
+    predictions = model.predict(samples)
+    inject_to_server(predictions, corpus)
 
-#url = 'https://hackernoon.com/a-guide-to-open-enrollment-and-the-health-insurance-marketplace-931h32j1'
-url = sys.argv[1]
-
-try:
-    article = Article(url)
-    article.download()
-    article.parse()
-except:
-    print('URL Error')
-    sys.stdout.flush()
-    exit()
-f = open('article.txt', 'w')
-f.write(article.text)
-f.close()
-
-lines = []
-fp = open('article.txt', 'r')
-for line in fp:
-    lines.append(line + '\n')
-fp.close()
-
-loaded_vec = TfidfVectorizer(stop_words='english',lowercase=True,vocabulary=pickle.load(open("feature.pkl", "rb")))
-
-corpus = open("article.txt")
-transformed2 = loaded_vec.fit_transform(corpus)
-with open("trial.pkl", 'rb') as file:
-    pickle_model = pickle.load(file)
-pred = pickle_model.predict(transformed2)
-
-count = 0
-for label in pred:
-    if label == 'propaganda':
-        print(lines[count] + '\n')
-    count += 1
-
-sys.stdout.flush()
-exit()
+if __name__ == "__main__":
+    Main()
