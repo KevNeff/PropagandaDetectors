245798554846d41c1e0b02cbe67019a186d13a1f
KevNeff <kevin.mcneff@gmail.com>
2019-12-07 02:56:38 -0600

    Updated Error handling in result POST

--- website/server.js
+++ website/server.js
@@ -12,32 +12,31 @@ app.get('/', (req, res) => {
 
     });
 });
-app.post('/result', function (req, res) {
-    var weblink=req.body.weblink;
-    var prediction="Invalid";
-    if (weblink == ""){
-        res.redirect('/');
-        console.log(prediction)
-        res.render('results',{
-            title:'Propaganda Detector Results',
-            weblink: weblink,
-            prediction: prediction
-        });
-    }
-    else {
-            const pythonProcess = spawn('C:\\Users\\Kevin\\AppData\\Local\\Programs\\Python\\Python37\\python.exe', ["C:/Users/Kevin/Desktop/propaganda-website/webparser.py", weblink]);
-            pythonProcess.stdout.on('data', (data) => {
-                prediction = data.toString()
-                res.render('results',{
-                    title:'Propaganda Detector Results',
-                    weblink: weblink,
-                    prediction: prediction
-                });
-            });
-
-    }
-    console.log('test')
+app.post('/result', function (req, res){
+   var weblink = req.body.weblink;
+   var prediction ='No URL';
+   if (weblink == ''){
+       res.render('results', {
+           title: 'Propaganda Detector Results',
+           weblink: weblink,
+           prediction: prediction
+       });
+   }
+   else {
+       const pythonProcess = spawn('C:\\Users\\Kevin\\AppData\\Local\\Programs\\Python\\Python37\\python.exe', ["C:/Users/Kevin/Desktop/propaganda-website/webparser.py", weblink]);
+       pythonProcess.stdout.on('data', (data) => {
+           prediction = data.toString()
+           if (prediction == '')
+               prediction = 'None found!'
+           res.render('results', {
+               title: 'Propaganda Detector Results',
+               weblink: weblink,
+               prediction: prediction
+           });
+       });
+   }
 });
+
 app.get('/result', function (req, res){
     res.redirect('/');
 });
